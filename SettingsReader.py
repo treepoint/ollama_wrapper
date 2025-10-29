ad2f10d052e1f9428424be7c94e80c9a8fa39c20
tree_point <79194544428@ya.ru>
2025-10-29 13:34:04 +0500

    remove exceeded functions

--- SettingsReader.py
+++ SettingsReader.py
@@ -3,11 +3,8 @@ import json
 from types import SimpleNamespace
 
 class SettingsReader():
-    def __init__(self, path, to_merge = None):
+    def __init__(self, path):
         self.settings = self.read_and_set_settings(path)
-
-        if to_merge:
-            self.settings = self.merge_settings(to_merge, self.settings)
 
     def read_and_set_settings(self, path):
         with open(path, 'r', encoding='utf-8') as f:
@@ -16,14 +13,6 @@ class SettingsReader():
             settings = SimpleNamespace(**settings)
 
         return settings   
-
-    def merge_settings(self, ollama, module):
-        ollama_dict = vars(ollama)
-        dictmodule_dict = vars(module)
-
-        merged = {**ollama_dict, **dictmodule_dict}
-
-        return SimpleNamespace(**merged)
     
     def get_settings(self):
         return self.settings
